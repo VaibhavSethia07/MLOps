56bca2b26b1b346bbeb378c62f04a3a6c2ec369c
Vaibhav Sethia <E19CSE321@bennett.edu.in>
2024-01-27 03:44:14 +0000

    dvc: configure data version control

---
+++ dvc/train.py
@@ -0,0 +1,63 @@
+import logging
+
+import hydra
+import pandas as pd
+import pytorch_lightning as pl
+import torch
+import wandb
+from data import DataModule
+from model import ColaModel
+from omegaconf.omegaconf import OmegaConf
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.loggers import WandbLogger
+
+logger = logging.getLogger(__name__)
+
+
+class SamplesVisualisationLogger(pl.Callback):
+    def __init__(self, datamodule):
+        super().__init__()
+
+        self.datamodule = datamodule
+
+    def on_validation_end(self, trainer, pl_module):
+        val_batch = next(iter(self.datamodule.val_dataloader()))
+        sentences = val_batch['sentence']
+
+        outputs = pl_module(val_batch['input_ids'], val_batch['attention_mask'])
+        preds = torch.argmax(outputs[0], -1)
+        labels = val_batch['label']
+
+        df = pd.DataFrame({'Sentence': sentences, 'Label': labels.numpy(), 'Predicted': preds.numpy()})
+
+        wrong_df = df[df['Label'] != df['Predicted']]
+        trainer.logger.experiment.log({
+            'examples': wandb.Table(dataframe=wrong_df, allow_mixed_types=True),
+            'global_step': trainer.global_step
+        })
+
+
+@hydra.main(config_path='./configs', config_name='config')
+def main(cfg):
+    logger.info(OmegaConf.to_yaml(cfg, resolve=True))
+    logger.info(f"Using the model: {cfg.model.name}")
+    logger.info(f"Using the tokenizer: {cfg.model.tokenizer}")
+
+    cola_data = DataModule(cfg.model.tokenizer, cfg.processing.batch_size, cfg.processing.max_length)
+    cola_model = ColaModel(cfg.model.name)
+
+    checkpoint_callback = ModelCheckpoint(dirpath='./models', filename="best-checkpoint.ckpt",
+                                          monitor='valid/loss', mode='min')
+    early_stopping_callback = EarlyStopping(monitor='valid/loss', patience=3, verbose=True, mode='min')
+
+    # Create a WandbLogger instance
+    wandb_logger = WandbLogger(project='MLOps', entity='renegade07')
+    trainer = pl.Trainer(max_epochs=cfg.training.max_epochs, logger=wandb_logger,
+                         log_every_n_steps=cfg.training.log_every_n_steps, deterministic=cfg.training.deterministic,
+                         callbacks=[checkpoint_callback, SamplesVisualisationLogger(cola_data), early_stopping_callback])
+    trainer.fit(model=cola_model, datamodule=cola_data)
+
+
+if __name__ == '__main__':
+    main()
