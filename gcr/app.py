682a8663515f9c8cec9820eb35274a87c54bfbf3
Vaibhav Sethia <E19CSE321@bennett.edu.in>
2024-02-10 07:38:08 +0000

    cloud: update docker file to use GCS as model storage

---
+++ gcr/app.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from fastapi import FastAPI
+from inference_onnx import ColaONNXPredictor
+from train import main
+
+summary = '''This is an application powered by NLP. 
+This app can classify a sentence into one of the two classes.
+❌ Unacceptable: Grammatically not correct
+✅ Acceptable: Grammatically correct
+'''
+app = FastAPI(title='MLOps App', summary=summary)
+# load the model
+predictor = ColaONNXPredictor('./models/model.onnx')
+
+
+@app.get('/')
+def home():
+    return summary
+
+
+@app.get('/predict')
+async def get_prediction(text: str):
+    result = predictor.predict(text)
+
+    return result
