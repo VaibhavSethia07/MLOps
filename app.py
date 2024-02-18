f4fa0e03227f48457f030cd2ee548e5246aa89b4
Vaibhav Sethia <E19CSE321@bennett.edu.in>
2024-02-18 09:48:49 +0000

    chore: fix typo in OpenTelemetry tracer

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from typing import List
 from fastapi import FastAPI
 from inference_onnx import ColaONNXPredictor
 from opentelemetry import trace
-from opentelemetry.sdk.trace import TraceProvider
+from opentelemetry.sdk.trace import TracerProvider
 from train import main
 
-provider = TraceProvider()
+provider = TracerProvider()
 trace.set_tracer_provider(provider)
-tracer = tracer.get_tracer(__name__)
+tracer = trace.get_tracer(__name__)
 
 summary = '''This is an application powered by NLP. 
 This app can classify a sentence into one of the two classes.
